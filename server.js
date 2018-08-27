61c9523226dd0420099a3c15de5bb463c1866817
Shane Smyth <shanesmyth8@gmail.com>
2018-08-26 20:57:27 -0500

    updates

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var http        = require('http');
 var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
-var sports    = require('./routes/sports');
 
 var app = express();
 
@@ -16,10 +15,6 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use(express.methodOverride());
-//app.use(express.favicon());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,25 +23,73 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
-// HubExchange Routes
-//app.get('/', routes.index );
 app.get('/', function(request, response, next) {
-});
-
-
-app.get('/load', function(request, response, next) {
-  response.render('load.ejs', {
+  response.render('index.ejs', {
   });
 });
 
-app.get('/sportsBlock', function(request, response, next) {
-  response.render('sportsBlock.ejs', {
-  });
+app.get('/getAsset', function(req, res, next) {
+  /**********************/
+  // CALL FOR AUTHORIZATION
+  /**********************/
+  var assetId = req.query.id;
+  console.log(assetId);
+  function authToken(clientId, clientSecret, assetId){
+    var options = {
+        url: 'http://auth.exacttargetapis.com/v1/requestToken',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        form: {'clientId': clientId, 'clientSecret': clientSecret}
+    }
+
+    request.post(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log('Bearer: Sucess');
+            let json = JSON.parse(body);
+            console.log(json);
+            var accessToken = json.accessToken;
+            console.log(accessToken);
+            getAsset(accessToken, assetId);
+        }else{
+            console.log('Auth Error');
+            res.json({ message: '<p style="color:red;text-align:center;">Authentication errored, validate clientId & clientSecret are correct.</p>' });
+        }
+    })
+  }
+
+  /**********************/
+  // POST DATA
+  /**********************/
+  function getAsset(accessToken, assetId){
+    var optionsAsset = {
+        url: 'https://www.exacttargetapis.com/asset/v1/content/assets/'+assetId,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+accessToken
+        }
+    }
+    console.log(optionsAsset);
+
+    request(optionsAsset, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            let jsonAsset = JSON.parse(body);
+            var content = jsonAsset.content;
+            console.log(content);
+            res.json({ message: content });
+        }else{
+            console.log('Post to DE: error');
+            console.log(body);
+            res.json({ message: '<p style="color:red;text-align:center;">Content Block Retrieval Error, verify content block exists.</p>' });
+        }
+    })
+  }
+  var auth = authToken(process.env.clientId, process.env.clientSecret, assetId);
+  
 });
-
-app.post('/save', routes.save );
-
-app.post('/sportsEndPoint', sports.sportsEndPoint );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Shanes Express server listening on port ' + app.get('port'));
