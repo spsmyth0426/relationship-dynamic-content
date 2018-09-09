86540dd7415c15f4276b903436b041f91da5f901
Shane Smyth <shanesmyth8@gmail.com>
2018-09-09 16:20:59 -0500

    mongodb

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var http        = require('http');
 var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
+var mongodb = require("mongodb");
+
+var CONTACTS_COLLECTION = "instances";
 
 var app = express(); 
 
@@ -22,6 +25,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
+
+// Connect to the database before starting the application server.
+var db;
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
 
 app.get('/', function(request, response, next) {
   const { Client } = require('pg');
@@ -45,8 +75,29 @@ app.get('/', function(request, response, next) {
   });
 }); 
 
+app.get("/instances", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
 app.get('/configure', function(request, response, next) {
   response.render('configure.ejs', {
+  });
+}); 
+
+app.post('/configure', function(request, response, next) {
+  var newContact = req.body;
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
   });
 }); 
 
